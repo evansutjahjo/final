706316171078c1113fd6efddd46c61096093d219
evansutjahjo <evansutjahjo@gmail.com>
2024-06-20 01:05:30 +0800

    dummy podcast card, podcast container

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { podcastData } from '@/constants'
+import PodcastCard from '@/components/PodcastCard'
 
 
 const Home = () => {
@@ -7,7 +9,17 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'> Trending Podcast</h1>
-
+        <div className='podcast_grid'>
+          {podcastData.map(({id, title, description, imgURL})=> (
+          <PodcastCard
+          key={id}
+          imgURL={imgURL}
+          title={title}
+          description={description}
+          podcastId={id}
+          />
+        ))}
+        </div> 
       </section>
     </div>
   )
