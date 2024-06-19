e57f6ff2877f1fe290c53ed02f6dce3e9ebc1816
evansutjahjo <evansutjahjo@gmail.com>
2024-06-19 21:49:07 +0800

    left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from "@/components/LeftSidebar";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,7 @@ export default function RootLayout({
   return (
 <div>
   <main>
-    <p className="text-white-1"> Left Sidebar</p>
+    <LeftSidebar />
     {children}
     <p className="text-white-1"> Right Sidebar</p>
   </main>
